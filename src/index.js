ad50e02538090f9d24aece6c9bc90980224bafd5
Florian <hi@florianpanchout.com>
2021-06-08 15:41:09 +0200

    Service Worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+//import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-serviceWorkerRegistration.register()
+//serviceWorkerRegistration.register()
+
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+  for (let registration of registrations) {
+    registration.unregister()
+  }
+})
